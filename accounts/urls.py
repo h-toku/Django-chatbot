3130b582a8ffc0c03ec19c8f27d4fe3e85173970
h-toku <tokuhara.nh@gmail.com>
2025-04-15 18:36:25 +0900

    ok

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('account/delete/', views.account_delete, name='account_delete'),
     path('password_reset/', CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+        ), name='password_reset_done'),
 ]
