ef8c8f6c6efe129892a2e4813c1a631764b1133a
h-toku <tokuhara.nh@gmail.com>
2025-04-16 18:21:02 +0900

    ok

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.chat_page, name='chat_page'),  # チャット画面の表示
+    path('', home, name='home'),  # チャット画面の表示
     path('api/chat/', views.chat, name='chat_api'),  # APIエンドポイント
     path('chat/', views.chat, name='chat'),
 ]
