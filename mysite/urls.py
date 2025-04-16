3e3fc83da30ff4285a2adc26cbf1b981183a1123
h-toku <tokuhara.nh@gmail.com>
2025-04-16 18:30:44 +0900

    ok

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from chatbot import views
+from chatbot.views import home
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('chat/', include('chatbot.urls')),
     path('accounts/', include('accounts.urls')),
